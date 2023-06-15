6f99148f708e483fd86a234a62a861c71e1c7143
David Cai <davidcai2012@gmail.com>
2023-06-15 11:26:57 -0500

    Add default to splits format switch statement

--- functions/parser/formatter.js
+++ functions/parser/formatter.js
@@ -360,6 +360,10 @@ function printSets(sets, printConfig=defaultFormatConfig) {
         } else {
           setSplits = determineSetSplits(set, formatPrinter, printConfig);
         }
+      default:
+        setSplits = determineSetSplits(set, formatPrinter, printConfig);
+        break;
+
     }
 
     fullDescription += `⏱️ ${setNameForAverage} ${setAverage}${setSplits === `` ? "" : "\n"}${setSplits}\n\n`;
