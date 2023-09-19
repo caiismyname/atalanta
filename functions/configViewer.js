ca2fed9faede9bc07cba9a3864cb93e24bc22a99
David Cai <davidcai2012@gmail.com>
2023-09-19 10:53:39 -0400

    Add strava connected count

--- functions/configViewer.js
+++ functions/configViewer.js
@@ -2,14 +2,15 @@ const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
 const firebase = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  // databaseURL: "https://atalanta-12c63-default-rtdb.firebaseio.com",
-  databaseURL: "http://127.0.0.1:9000/?ns=atalanta-12c63-default-rtdb",
+  databaseURL: "https://atalanta-12c63-default-rtdb.firebaseio.com",
+  // databaseURL: "http://127.0.0.1:9000/?ns=atalanta-12c63-default-rtdb",
 });
 const db = firebase.database();
 
 db.ref(`users`).once("value", (snapshot) => {
   const allUsers = snapshot.val();
   const combinedConfigs = {};
+  var stravaConnectedCount = 0;
 
   Object.keys(allUsers).forEach((userID) => {
     const userFormatConfig = allUsers[userID]["preferences"]["format"];
@@ -25,9 +26,13 @@ db.ref(`users`).once("value", (snapshot) => {
         combinedConfigs[option][value] = 1;
       }
     }
+    if (allUsers[userID]["stravaConnected"]){
+      stravaConnectedCount += 1;
+    }
   });
 
   console.log(JSON.stringify(combinedConfigs, null, 4));
+  console.log(`Strava connected count: ${stravaConnectedCount}`);
   console.log(`Total user count: ${Object.keys(allUsers).length}`);
   firebase.delete(); // This terminates the process. Otherwise the script never ends
 });
